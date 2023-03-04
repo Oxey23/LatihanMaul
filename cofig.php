65024dbf0acb371c7dedcb8d5f38ed8637e6a1a4
Oxey23 <maulana.gowrk90@gmail.com>
2023-03-04 19:23:29 +0700

    Pekerjaan Kedua

---
+++ cofig.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$db = 'db_siswa';
+$user = 'root';
+$pass = '';
+
+$conn = new PDO('mysql:host=' . $host . 'dbname=' . $db . '', $user . $pass);
---
+++ pegawai.php
@@ -0,0 +1,17 @@
+<?php
+
+class pegawai
+{
+    public static function list_pegawai()
+    {
+        $list_pegawai = ['Okki', 'Adnir'];
+        return $list_pegawai;
+    }
+
+    public static function save_pegawai()
+    {
+    }
+    public static function delete()
+    {
+    }
+}
